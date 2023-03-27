8d79597800e8740528ac0bb995369b2e7edb2ca0
Martin <MartinVahala@seznam.cz>
2023-03-27 14:59:24 +0200

    Optimization

--- src/main/java/cz/mvsoft/controller/RESTFilmsController.java
+++ src/main/java/cz/mvsoft/controller/RESTFilmsController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import cz.mvsoft.entity.entertainment.Film;
 import cz.mvsoft.service.FilmsService;
--- src/main/java/cz/mvsoft/entity/entertainment/GameStudio.java
+++ src/main/java/cz/mvsoft/entity/entertainment/GameStudio.java
@@ -1,15 +1,10 @@
 package cz.mvsoft.entity.entertainment;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
